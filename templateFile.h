463d635b0d562531f1c4371b382444949896c56a
Akshat Ajay <akshat.ajay0931@gmail.com>
2022-06-12 00:16:22 +0530

    Updated

---
+++ templateFile.h
@@ -0,0 +1,17 @@
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpii;
+
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define SQ(a) (a)*(a)
+#define TC(t) while (t--)
+
+
+
